25961d59918cf1a65eed2b4ff0afe0c30d8b1351
Allan <kipropsallan@gmail.com>
2022-08-23 09:15:28 +0300

    commit tree 3230773f664c875028fdbbface7295146a3b1863

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
 
+   sentences = 0
+   punctuation = [".", "?", "!"]
+    self.split(" ").each do |word|
+      if punctuation.include?(word[-1])
+        sentences += 1
+      end
+
+    end 
+    sentences
   end
+  
 end
